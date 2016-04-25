8cc7fe5c49b2b4745c817ab3803103e97a82724b
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2016-04-25 12:24:21 +0100

    Documentacao do Way acrescentada

--- src/Way.h
+++ src/Way.h
@@ -14,6 +14,9 @@
 
 using namespace std;
 
+/**
+ * Classe Way que gere as informações das arestas
+ */
 class Way {
 
 	unsigned int id;
@@ -27,13 +30,45 @@ public:
 	Way(int id, string name, Transport::Type type);
 	virtual ~Way();
 
+	/**
+	 * Funcao get para o Id de Way
+	 * @return o Id de Way
+	 */
 	unsigned int getID() const;
+	/**
+	 * Funcao get para o nome do Way
+	 * @return nome do Way
+	 */
 	string getName() const;
+	/**
+	 * Funcao get para o preço por metro mediante o tipo de transporte
+	 * @return o preço do metro
+	 */
 	double getPricePerMeter() const;
+	/**
+	 * Funcao get para a distancia do Way
+	 * @return a distancia do Way
+	 */
 	double getDistance() const;
+	/**
+	 * Funcao set para a distancia do Way
+	 * @param dist a nova distancia do Way
+	 */
 	void setDistance(double dist);
+	/**
+	 * Funcao get para o tempo mediante o tipo de transporte
+	 * @return o tempo de transição do Way
+	 */
 	double getTime() const;
+	/**
+	 * Funcao get para o preço
+	 * @return o custo do Way
+	 */
 	double getPrice() const;
+	/**
+	 * Funcao get para o tipo de transporte do Way
+	 * @return o tipo de transporte
+	 */
 	Transport::Type getType() const;
 };
 
